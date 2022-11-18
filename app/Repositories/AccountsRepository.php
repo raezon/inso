b94d088c6c171b10952a8f6b71b08b1358d3c648
raezon <amardjebabla10@gmail.com>
2022-11-18 21:52:25 +0100

    enhance

--- app/Repositories/AccountsRepository.php
+++ app/Repositories/AccountsRepository.php
@@ -23,7 +23,8 @@ class AccountsRepository implements AccountsRepositoryInterface
 
     public function getByUuid($uuid)
     {
-        return Accounts::where(['uuid', $uuid]);
+
+        return Accounts::where('uuid','=', $uuid);
     }
     
 
